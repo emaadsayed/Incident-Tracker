b79f240391cb399a5ed191f84e347bdc2c023e73
Emaad <sayedemaad@gmail.com>
2026-02-18 22:29:14 +0530

    frontend env added

--- frontend/src/api/incidentApi.js
+++ frontend/src/api/incidentApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api/incidents";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const createIncident = async (data) => {
   const res = await fetch(BASE_URL, {
